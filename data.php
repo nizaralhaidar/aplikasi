afed627bc4ea2f1152d17dd227116a40096f7e9c
nizaralhaidar <nizaralhaidar6@gmail.com>
2023-11-26 23:39:34 +0700

    first commit

--- data.php
+++ data.php
@@ -1,6 +1,86 @@
 <?php
 
 include_once("config.php");
+class User {
+    private $nama;
+    private $harga;
+    private $stok;
+
+    // Constructor
+    public function __construct($nama, $harga , $stok) {
+        $this->setNama($nama);
+        $this->setHarga($harga);
+        $this->setStok($stok);
+    }
+
+    // Getter and Setter for Username
+    public function getNama() {
+        return $this->nama;
+    }
+
+    public function setNama($nama) {
+        $this->nama = $nama;
+    }
+
+    // Getter and Setter for Password
+    public function getHarga() {
+        return $this->harga;
+    }
+
+    public function setHarga($harga) {
+        $this->harga = $harga;
+    }
+
+    public function getStok() {
+        return $this->stok;
+    }
+
+    public function setStok($stok) {
+        $this->stok = $stok;
+    }
+
+
+    public function notification() {
+        echo "nama: " . $this->getNama() . "</br>\n";
+        echo "harga: " . $this->getHarga() . "</br>\n";
+        echo "stok: " . $this->getStok() . "</br>\n";
+        echo "User created by User </br>\n";
+    }
+}
+
+class Barang extends User {
+    private $role = "barang";
+
+    // Constructor
+    public function __construct($nama, $harga , $stok) {
+        parent::__construct($nama, $harga, $stok);
+    }
+
+    public function notification() {
+        echo "nama: " . $this->getNama() . "</br>\n";
+        echo "harga: " . $this->getHarga() . "</br>\n";
+        echo "stok: " . $this->getStok() . "</br>\n";
+        echo "User created by " . $this->role . "\n";
+    }
+
+    public function saveToDatabase() {
+        // Simpan data ke database
+        global $mysqli;
+        $nama = $this->getNama();
+        $harga = $this->getHarga();
+        $stok = $this->getStok();
+
+        $query = "INSERT INTO barang (name, harga, stok) VALUES (?, ?, ?)";
+        $stmt = mysqli_prepare($mysqli, $query);
+        mysqli_stmt_bind_param($stmt, "sss", $nama, $harga, $stok);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
+}
+
+$barangObjek = new Barang("Shampoo", "20000", "10");
+$barangObjek->saveToDatabase();
+
 $result = mysqli_query($mysqli, "SELECT * FROM barang ORDER BY id DESC");
 ?>
 
@@ -16,79 +96,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM barang ORDER BY id DESC");
         <tr>
             <th>Name</th><th>Harga</th><th>Stok</th><th>Update</th>
         </tr>
-        <?php
-        class User {
-            private $nama;
-            private $harga;
-            private $stok;
-        
-            // Constructor
-            public function __construct($nama, $harga , $stok) {
-                $this->setNama($nama);
-                $this->setHarga($harga);
-                $this->setStok($stok);
-            }
-        
-            // Getter and Setter for Username
-            public function getNama() {
-                return $this->nama;
-            }
-        
-            public function setNama($nama) {
-                $this->nama = $nama;
-            }
-
-            // Getter and Setter for Password
-            public function getHarga() {
-                return $this->harga;
-            }
-        
-            public function setHarga($harga) {
-                $this->harga = $harga;
-            }
-
-            public function getStok() {
-                return $this->stok;
-            }
-        
-            public function setStok($stok) {
-                $this->stok = $stok;
-            }
-        
-        
-            public function notification() {
-                echo "nama: " . $this->getNama() . "</br>\n";
-                echo "harga: " . $this->getHarga() . "</br>\n";
-                echo "stok: " . $this->getStok() . "</br>\n";
-                echo "User created by User </br>\n";
-            }
-        }
-        
-        class Barang extends User {
-            private $role = "barang";
-        
-            // Constructor
-            public function __construct($nama, $harga , $stok) {
-                parent::__construct($nama, $harga, $stok);
-            }
-        
-            public function notification() {
-                echo "nama: " . $this->getNama() . "</br>\n";
-                echo "harga: " . $this->getHarga() . "</br>\n";
-                echo "stok: " . $this->getStok() . "</br>\n";
-                echo "User created by " . $this->role . "\n";
-
-        // Simpan data ke database
-        global $mysqli;
-        $nama = $this->getNama();
-        $harga = $this->getHarga();
-        $stok = $this->getStok();
-
-        $query = "INSERT INTO barang (name, harga, stok) VALUES ('$nama', '$harga', '$stok')";
-        mysqli_query($mysqli, $query);
-            }
-        }
-        
+         <?php
         while ($barang_data = mysqli_fetch_array($result)) {
             echo "<tr>";
             echo "<td>".$barang_data['name']."</td>";
